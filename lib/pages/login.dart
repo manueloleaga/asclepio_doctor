6e079b42c3f87904cfd54f12eb2856a736caa4c2
Manuel Oleaga <manuel.oleaga1@gmail.com>
2020-04-27 19:55:46 -0400

    - Creacion de tab menu, y parte del login

---
+++ lib/pages/login.dart
@@ -0,0 +1,31 @@
+import 'package:asclepio_flutter/utilities/constant.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kWhiteColor,
+      resizeToAvoidBottomPadding: true,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 20.0),
+                child: Image.asset(""),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/utilities/constant.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+//Colors
+const kMainGreenColor = Color(0xFF00BFA6);
+const kWhiteColor = Color(0xFFFFFFFF);
