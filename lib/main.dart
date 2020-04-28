1ee6da688e247fd3c9bdeaf41733dedbd78439d0
Manuel Oleaga <manuel.oleaga1@gmail.com>
2020-04-28 15:19:30 -0400

    - Creacion de pantalla login diseño

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:asclepio_flutter/pages/login.dart';
-import 'package:asclepio_flutter/pages/tabPages.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: TabPages(),
+      home: Login(),
       //Login
     );
   }
